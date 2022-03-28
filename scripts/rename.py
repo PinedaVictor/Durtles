f12099a154624a7ad48707e9296596874b356026
unknown <pinedavictor095@gmail.com>
2022-03-28 14:11:57 -0700

    Init commit

---
+++ scripts/rename.py
@@ -0,0 +1,37 @@
+# 
+# 
+# Victor Pineda
+
+import re 
+import os
+
+
+print("Wassup")
+
+currentDir = os.getcwd()
+print("Current Dirctory: " + currentDir)
+editDir = "{}/Background".format(currentDir)
+print("Edit Dir: " + editDir)
+
+def reNameImages():
+    print("Hi from rename funciton")
+    pattern = re.compile("[a-zA-Z]*-?[a-zA-Z]*[#][(\d+(?:\.\d+)?)]*[-]?.[a-zA-Z]*")
+    removeUndeDash = "_*-*"
+
+    for filename in os.listdir(editDir):
+        # print("File Name: " + filename)
+        src = "{0}/{1}".format(editDir, filename)
+        findPattern = pattern.findall(filename)
+        tempName = ''.join(findPattern)
+        newName = re.sub(removeUndeDash, '', tempName)
+        print("New name: " + newName)
+        dist = "{0}/{1}".format(editDir, newName)
+        # print(dist)
+        os.rename(src, dist)
+        
+            
+
+reNameImages()
+
+def countFiles():
+    print(len(os.listdir(currentDir)))
