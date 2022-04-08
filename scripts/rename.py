5f1cd7c7e6be28b3ddd37d8d7d2625e84605313a
PinedaVictor <pinedavictor095@gmail.com>
2022-04-08 16:50:47 -0700

    Added count folders script, and accepter function for rename script

--- scripts/rename.py
+++ scripts/rename.py
@@ -1,37 +1,36 @@
 # 
 # Victor Pineda
-# 
+# Description: This will rename all files in the 
+# Layers directory with the following pattern.
 
 import re 
 import os
-
+import sys
+import getopt
 
 currentDir = os.getcwd()
-# print("Current Dirctory: " + currentDir)
-# editDir = "{}/Background".format(currentDir)
-# print("Edit Dir: " + editDir)
-
 pattern = re.compile("[a-zA-Z]* ?-?[a-zA-Z]* ?-?[\s]?[a-zA-Z]*[#][(\d+(?:\.\d+)?)]*[-]?.[a-zA-Z]*")
 removeUndeDash = "_*-*"
 
-# def reNameImages():
-#     print("Hi from rename funciton")
 
-#     for filename in os.listdir(editDir):
-#         # print("File Name: " + filename)
-#         src = "{0}/{1}".format(editDir, filename)
-#         findPattern = pattern.findall(filename)
-#         tempName = ''.join(findPattern)
-#         newName = re.sub(removeUndeDash, '', tempName)
-#         print("New name: " + newName)
-#         dist = "{0}/{1}".format(editDir, newName)
-#         # print(dist)
-#         os.rename(src, dist)
+def rename():
+    parentPath = os.path.dirname(currentDir)
+    print("Parent directory: " + parentPath)
+    editingDir = "{}/Layers".format(parentPath)
+    print("Editing Directory: " + editingDir)
 
-# reNameImages()
+    for subDir, subFolderName, files in os.walk(editingDir):
+        for file in files:
+            src = "{0}/{1}".format(subDir, file)
+            findPattern = pattern.findall(file)
+            tempName = ''.join(findPattern)
+            newName = re.sub(removeUndeDash, '', tempName)
+            dist = "{0}/{1}".format(subDir, newName)
+            # TODO: Uncomment when ready
+            # os.rename(src, dist)
 
 
-def loopSubDirs():
+def previewRenameEdit():
     print("In loopSubDirs")
     parentPath = os.path.dirname(currentDir)
     print("Parent Dir: " + parentPath)
@@ -39,7 +38,7 @@ def loopSubDirs():
     print("loppSubDirs: " + editingDir)
 
     for subDir, subFolderName, files in os.walk(editingDir):
-        print("In my directory loop: " + str(len(subFolderName)) + " SubDirs: " + subDir)
+        print("In my directory loop: " + subDir)
         for file in files:
             print("The file: " + file)
             src = "{0}/{1}".format(subDir, file)
@@ -50,15 +49,28 @@ def loopSubDirs():
             print("New name: " + newName)
             dist = "{0}/{1}".format(subDir, newName)
             print("New dist: " + dist)
-            os.rename(src, dist)
             print("      ")
-
         print("--")
 
-loopSubDirs()
 
-def countFiles():
-    print(len(os.listdir(currentDir)))
+def accepter():
 
+    argLength = len(sys.argv)
 
-# countFiles()
+    if(argLength == 1):
+        print("No args other than sript name")
+        rename()
+    else:
+        try:
+            args = str(sys.argv)
+            print("Without str function: " +  sys.argv[1])
+            print("the args: " + args)
+            tie, idk = getopt.getopt(args, '-v -b')
+            print(tie)
+            print("idk "+ idk)
+        except:
+            print("Error occured: Make sure your input is in the correct format ")
+
+    
+
+accepter()
