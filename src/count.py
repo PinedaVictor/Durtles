6e96125489e5310cd9172185ed008d58e8e7ec44
PinedaVictor <pinedavictor095@gmail.com>
2022-04-27 18:00:36 -0700

    Manully parsing cmd arguments

--- src/count.py
+++ src/count.py
@@ -3,26 +3,19 @@
 # Description: List all folders in the Layers directory.
 
 import os
-import typer
 from global_ import EDITING_DIR
 
-app = typer.Typer()
 
-
-@app.command("d")
-def count_folders_dir(g: str = typer.Option("", help="SEE what it is")):
+def count_folders_dir():
     """
      Count number of folders in a directory.
     """
     try:
         num_of_folders = len(os.listdir(EDITING_DIR))
-        typer.secho(num_of_folders, fg=typer.colors.GREEN)
+        print(num_of_folders)
+        # typer.secho(num_of_folders, fg=typer.colors.GREEN)
     except:
         if not os.path.isdir(EDITING_DIR):
             print("Directory does not exist")
         else:
             print("Error has occurred")
-
-
-if __name__ == "__main__":
-    app()
--- src/main.py
+++ src/main.py
@@ -1,27 +1,39 @@
 # Main function
-import typer
-import count
-from rename import preview_rename
-
-app = typer.Typer()
-app.add_typer(count.app, name="count")
+import sys
+from count import count_folders_dir
+# import count
+# from rename import preview_rename
 
 
-@app.command("rename")
-def handle_rename(op: str = typer.Option("su")):
-    # TODO: Define whether directory will be user input or defined globally
-    """
-    Renaming files within in a directory.
-    """
-    if(op == "p"):
-        preview_rename()
+OPTIONS = {"-help", "-V", "-c"}
 
 
 def main():
-    """
-    Directory utilities that automate time-consuming tasks.
-    """
+    arg_length = len(sys.argv)
+
+    # Check if their ar no args
+    if(arg_length == 1):
+        print("drt: try drt --help")
+    else:
+        input_option = sys.argv[1]
+
+    # Check if it exists in the
+        if(input_option in OPTIONS):
+            print(input_option)
+            print("Option exists")
+            # Run function based on option input
+            if(input_option == "-help"):
+                print("the help")
+            elif(input_option == "-c"):
+                print("calling count")
+                count_folders_dir()
+                # print_help()
+
+        else:
+            print(input_option)
+            print(OPTIONS)
+            print("Option does not exists: Use --help for availble option")
 
 
 if __name__ == '__main__':
-    app()
+    main()
